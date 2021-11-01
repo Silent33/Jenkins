742db8917a227514db86b1ff7f39f62a5f05e617
Oleksii Iskiv <oleksi.iskiv@perfectial.com>
2021-11-01 16:26:39 +0200

    message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and HELLO WORLD 1234
+          Learn React and HELLO
         </a>
       </header>
     </div>
